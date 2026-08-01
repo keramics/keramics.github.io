db16e06ea140ede2dadfd3cb535e436687eab68e
Joachim Metz <joachim.metz@gmail.com>
2026-08-01 18:41:35 +0200

    Updated Keramics data format specifications

--- docs/searcher-09f2665d.js
+++ docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4fcfef61.js',
+                path_to_root + 'searchindex-503c0708.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
